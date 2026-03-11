367a4fd11bb5263a9608410ec0bc91c6a62cd3cc
Jonny0406 <tafeka0406@gmail.com>
2026-03-10 20:31:20 -0600

    Correcciones Hechas

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,11 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework.viewsets import ModelViewSet
 
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import permission_classes
-
 #   Importacion de modelos y serializers reales
 
 from apps.models.models import (
